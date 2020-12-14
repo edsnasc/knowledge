92b540fad8f1be4afeba8e71c1dc83174d948c84
edsnasc <edsnascimento89@gmail.com>
2020-12-13 21:53:54 -0300

    updating front

---
+++ frontend/src/components/templates/Content.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="content"></div>
+</template>
+
+<script>
+export default {
+    name: 'Content'
+}
+</script>
+
+<style>
+    .Content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
