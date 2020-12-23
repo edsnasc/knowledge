3800eb5924699ae8477805485ad81b26aaa27261
edsnasc <edsnascimento89@gmail.com>
2020-12-22 23:39:38 -0300

    create page categoryAdmin

--- frontend/src/components/admin/CategoryAdmin.vue
+++ frontend/src/components/admin/CategoryAdmin.vue
@@ -1,34 +1,35 @@
 <template>
-  <div class="category-admin">
+    <div class="category-admin">
         <b-form>
             <input id="category-id" type="hidden" v-model="category.id" />
-            <b-row>
-                <b-col xs="12">
-                    <b-form-group label="Nome:" label-for="category-name">
-                        <b-form-input id="category-name" type="text"
-                            v-model="category.name" required
-                            :readonly="mode === 'remove'"
-                            placeholder="Informe o Nome do Categoria..." />
-                    </b-form-group>
-                </b-col>
-            </b-row>
-            <b-row>
-                <b-col xs="12">
-                    <b-button variant="primary" v-if="mode === 'save'"
-                        @click="save">Salvar</b-button>
-                    <b-button variant="danger" v-if="mode === 'remove'"
-                        @click="remove">Excluir</b-button>
-                    <b-button class="ml-2" @click="reset">Cancelar</b-button>
-                </b-col>
-            </b-row>
+            <b-form-group label="Nome:" label-for="category-name">
+                <b-form-input id="category-name" type="text"
+                    v-model="category.name" required
+                    :readonly="mode === 'remove'"
+                    placeholder="Informe o Nome da Categoria..." />
+            </b-form-group>
+            <b-form-group label="Categoria Pai:" label-for="category-parentId">
+                <b-form-select v-if="mode === 'save'"
+                    id="category-parentId"
+                    :options="categories" v-model="category.parentId" />
+                <b-form-input v-else
+                    id="category-parentId" type="text"
+                    v-model="category.path"
+                    readonly />
+            </b-form-group>
+            <b-button variant="primary" v-if="mode === 'save'"
+                @click="save">Salvar</b-button>
+            <b-button variant="danger" v-if="mode === 'remove'"
+                @click="remove">Excluir</b-button>
+            <b-button class="ml-2" @click="reset">Cancelar</b-button>
         </b-form>
         <hr>
-        <b-table hover striped :items="category" :fields="fields">
+        <b-table hover striped :items="categories" :fields="fields">
             <template slot="actions" slot-scope="data">
-                <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+                <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2">
                     <i class="fa fa-pencil"></i>
                 </b-button>
-                <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+                <b-button variant="danger" @click="loadCategory(data.item, 'remove')">
                     <i class="fa fa-trash"></i>
                 </b-button>
             </template>
@@ -44,34 +45,35 @@ export default {
     data: function() {
         return {
             mode: 'save',
-            user: {},
-            category: [],
+            category: {},
+            categories: [],
             fields: [
                 { key: 'id', label: 'Código', sortable: true },
                 { key: 'name', label: 'Nome', sortable: true },
-                { key: 'email', label: 'E-mail', sortable: true },
-                { key: 'admin', label: 'Administrador', sortable: true,
-                    formatter: value => value ? 'Sim' : 'Não' },
+                { key: 'path', label: 'Caminho', sortable: true },
                 { key: 'actions', label: 'Ações' }
             ]
         }
     },
     methods: {
-        loadCategory() {
-            const url = `${baseApiUrl}/users`
+        loadCategories() {
+            const url = `${baseApiUrl}/categories`
             axios.get(url).then(res => {
-                this.users = res.data
+                // this.categories = res.data
+                this.categories = res.data.map(category => {
+                    return { ...category, value: category.id, text: category.path }
+                })
             })
         },
         reset() {
             this.mode = 'save'
-            this.user = {}
-            this.loadUsers()
+            this.category = {}
+            this.loadCategories()
         },
         save() {
-            const method = this.user.id ? 'put' : 'post'
-            const id = this.user.id ? `/${this.user.id}` : ''
-            axios[method](`${baseApiUrl}/users${id}`, this.user)
+            const method = this.category.id ? 'put' : 'post'
+            const id = this.category.id ? `/${this.category.id}` : ''
+            axios[method](`${baseApiUrl}/categories${id}`, this.category)
                 .then(() => {
                     this.$toasted.global.defaultSuccess()
                     this.reset()
@@ -79,8 +81,8 @@ export default {
                 .catch(showError)
         },
         remove() {
-            const id = this.user.id
-            axios.delete(`${baseApiUrl}/users/${id}`)
+            const id = this.category.id
+            axios.delete(`${baseApiUrl}/categories/${id}`)
                 .then(() => {
                     this.$toasted.global.defaultSuccess()
                     this.reset()
@@ -99,5 +101,7 @@ export default {
 </script>
 
 <style>
-
+ .category-admin {
+        padding: 20px;
+    }
 </style>
