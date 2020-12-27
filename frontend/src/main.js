312706d12f5a34a3ed101e109f876bae03a2135b
edsnasc <edsnascimento89@gmail.com>
2020-12-27 18:17:56 -0300

    updating knowledge

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,12 +10,10 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// TEMPORARIO!
-
-    require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkVkc29uIE4uIE5hc2NpbWVudG8iLCJlbWFpbCI6ImVkc25hc2NpbWVudG84OUBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjA4NDcxOTk4LCJleHAiOjE2MDg3MzExOTh9._idvARuSaOeDzE-Z_zA6nojhzOPryD5i0Zah0Aj36TU' 
-
 new Vue({
   store,
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
